2d1700b4da0b244ca051df7da194e99f39c0a9ce
Arnav Roy <143030580+ArnavRoy520@users.noreply.github.com>
2024-07-03 14:55:25 +0530

    Update Instruction_Memory.v

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -1,1 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: -
+// Engineer: Arnav Roy
+// 
+// Create Date: 02.06.2024 07:58:22
+// Design Name: RISC-V
+// Module Name: Instruction_Memory
+// Project Name: RISC-V
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Instruction_Memory(
+    input rst,
+    input [31:0] A,
+    output [31:0] RD
+    );
+    reg [31:0] mem[1023:0];
+    
+   assign RD = (rst == 1'b0) ? {32{1'b0}} : mem[A[31:2]];
+
+  initial 
+  begin
+    $readmemh("Memory_log.mem",mem);
+  end
+
+endmodule
 
