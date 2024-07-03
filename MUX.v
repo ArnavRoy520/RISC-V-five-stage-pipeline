ba51535dbdbfae58879e2de8d329b845e116567f
Arnav Roy <143030580+ArnavRoy520@users.noreply.github.com>
2024-07-03 14:52:18 +0530

    Update MUX.v

--- MUX.v
+++ MUX.v
@@ -1,1 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: -
+// Engineer: Arnav Roy
+// 
+// Create Date: 02.06.2024 07:21:22
+// Design Name: RISC-V
+// Module Name: MUX
+// Project Name: RISC-V
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUX(
+    input wire [31:0] a,
+    input wire [31:0] b,
+    input wire sel,
+    output wire [31:0] c
+    );
+    
+    assign c = sel ? b : a;
+endmodule
 
